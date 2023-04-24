804925f740e550a8f73294dd004a8d33b69c2f1c
Pranav Yogesh kangale <115502017+pranavkangale5@users.noreply.github.com>
2023-04-24 12:56:22 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLr2QUriRCpp2e1wz_003G424bWefyM5o",
+  apiKey: "API KEY",
   authDomain: "communityforum-cryptoverse.firebaseapp.com",
   databaseURL: "https://communityforum-cryptoverse-default-rtdb.firebaseio.com",
   projectId: "communityforum-cryptoverse",
   storageBucket: "communityforum-cryptoverse.appspot.com",
-  messagingSenderId: "926605131352",
-  appId: "1:926605131352:web:b23c462200f0b4db9d542d",
+  messagingSenderId: "ID",
+  appId: "APP ID",
 };
 
 const app = initializeApp(firebaseConfig);
